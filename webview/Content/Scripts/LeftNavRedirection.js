d3313735ded05cdcd686f078cb6d378df1044fef
NikhilJojare <68631256+NikhilJojare@users.noreply.github.com>
2020-07-22 12:14:58 +0530

    Add files via upload

---
+++ webview/Content/Scripts/LeftNavRedirection.js
@@ -0,0 +1,25 @@
+$(function () {
+    // Servicable
+    alert();
+    $('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home1.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/uploadSGRAskFile");
+    $('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home2.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/uploadSGRItemFile");
+    $('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home3.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/uploadMORItemFile");
+    //$('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home4.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/addSGRAskTask");
+    //$('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home5.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/addSGRItemTask");
+    //$('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home6.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/addMORItemTask");
+    $('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home4.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/addOrUploadASkItems");
+    $('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home5.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/addOrUploadSGRItems");
+    $('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home6.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/addOrUploadMORItems");
+    //$('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home7.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/userSGRAskByActionForm");
+    //$('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home13.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/userSGRAskToActionForm");    
+    $('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home10.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/SGRAskDashboard");
+    $('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home11.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/SGRActionDashboard");
+    $('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home12.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/MORActionDashboard");
+    //$('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/MEP/Pages/Home7.aspx"]').removeProp("href").attr("href", "/sites/dev_team/MEP/Pages/HomeMEP.aspx#/pivotReports");
+    
+    $('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home8.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/configDashboard");
+    $('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home9.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/SGRActionItemReport");
+    $('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home13.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/pendingItem");
+    $('#DeltaPlaceHolderLeftNavBar ul.root li.static a[href="/sites/dev_team/GlobalTracker/Pages/Home14.aspx"]').removeProp("href").attr("href", "/sites/dev_team/GlobalTracker/Pages/default.aspx#/ViewAllItems");
+
+ });
